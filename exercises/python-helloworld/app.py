d6611236b70a38c9f01cbecdfa3d3d58ed27dbc7
Jerilene Medrano <jerixmx@gmail.com>
2021-06-13 15:26:30 +0800

    Add logging at debug level function

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,8 +1,13 @@
-## Solution obtained through Udacity
+## Solution for endpoints obtained through Udacity
 
 from flask import Flask
 from flask import json
+import logging
+
 app = Flask(__name__)
+
+FORMAT = '%(asctime)s, %(funcName)s %(message)s'
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format = FORMAT)
 
 @app.route("/")
 def hello():
@@ -15,6 +20,7 @@ def status():
         status=200,
         mimetype='application/json'
     )
+    logging.debug('endpoint was reached')
     return response
 
 @app.route("/metrics")
@@ -28,6 +34,7 @@ def metrics():
         status=200,
         mimetype='application/json'
         )
+    logging.debug('endpoint was reached')
     return response
 
 if __name__ == "__main__":
